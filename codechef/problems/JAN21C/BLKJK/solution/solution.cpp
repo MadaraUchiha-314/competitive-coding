5d0b2bfa1e07d5baf5e1e4963dbf384c54f6cec5
MadaraUchiha-314 <rohithr31@gmail.com>
2021-01-08 23:59:50 +0530

    fix: blackjack done

--- codechef/problems/JAN21C/BLKJK/solution/solution.cpp
+++ codechef/problems/JAN21C/BLKJK/solution/solution.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG
   #define debug printf
@@ -14,85 +14,57 @@ using namespace std;
 int main() {
   int T, N, X, Y;
   int A[1000];
-  vector<int> cum_sum;
+  vector<int> sums(1005);
   scanf("%d", &T);
   while(T--) {
-    debug("Test case %d\n", T);
-    cum_sum.clear();
+    debug("========= Test case %d =========\n", T);
     scanf("%d %d %d", &N, &X, &Y);
-    cum_sum.push_back(0);
     for (int i = 0; i < N; i++) {
       scanf("%d", A + i);
-      cum_sum.push_back(cum_sum[i] + A[i]);
-    }
-    int lower_x = 0, lower_y = 0;
-    for (int i = 0; i < cum_sum.size(); i++) {
-      if (cum_sum[i] <= X) {
-        lower_x = max(lower_x, i);
-      }
-      if (cum_sum[i] <= Y) {
-        lower_y = max(lower_y, i);
+      if (i > 0) {
+        sums[i] = sums[i - 1] + A[i];
+      } else {
+        sums[i] = A[i];
       }
     }
-    /**
-     * Example array [7, 8, 5, 3, 2, 1]
-     * Cumulative sum array: [0, 7, 15, 20, 23, 25, 26]
-     * Case - 1:
-     * X = 17, Y = 19
-     * lower_x == lower_y == 2
-     */
-    /**
-     * lower_x and lower_y need to be exactly the same for us to be swapping.
-     * If not there is some cumulative sum which is in between.
-     */
-    debug("Lower x = %d Lower Y = %d and sums are %d %d\n", lower_x, lower_y, cum_sum[lower_x], cum_sum[lower_y]);
-    if (lower_x != lower_y) {
+    bool zero_swap = false, one_swap = false;
+    int total = 0;
+    for (int i = 0; i <  N; i++) {
+      total += A[i];
+      if (total >= X && total <= Y) {
+        zero_swap = true;
+        break;
+      }
+      for (int j = i + 1; j < N; j++) {
+        // We swap A[i] and A[j]
+        int delta = A[j] - A[i];
+        // Now all the elements from i to j have delta added to them.
+        int x_pos = upper_bound(sums.begin() + i, sums.begin() + j, X - delta) - sums.begin();
+        int y_pos = lower_bound(sums.begin() + i, sums.begin() + j, Y - delta) - sums.begin();
+        if (sums[x_pos] == X - delta || sums[y_pos] == Y - delta) {
+          one_swap = true;
+        } else  if (x_pos != y_pos) {
+          one_swap = true;
+        }
+        debug("Arrays is \n");
+        for (int k = 0; k < N; k++) debug("%d ", A[k]);
+        debug("\n");
+        debug("Cumulative Arrays is \n");
+        for (int k = 0; k < N; k++) debug("%d ", sums[k]);
+        debug("\n");
+        debug("Swapping %d at %d and %d at %d.\n", A[i], i, A[j], j);
+        debug("New Cumulative Arrays is \n");
+        for (int k = 0; k < N; k++) debug("%d ", k >= i && k <j ? sums[k] + delta : sums[k]);
+        debug("\n");
+        debug("Searched for %d and %d\n", X - delta, Y - delta);
+        debug("Found %d at %d and %d at %d\n", X - delta, x_pos, Y - delta, y_pos);
+      }
+    }
+    if (zero_swap) {
       printf("0\n");
-    } else if (cum_sum[lower_x] == X || cum_sum[lower_y] == Y) {
-      printf("0\n");
-    } else if (lower_x == cum_sum.size() - 1) {
-      printf("-1\n");
-    }
-    else {
-      /**
-       * There are 2 cases where we will require a swap of exactly 1.
-       * Case-1, we swap one of the elements from the left of lower_x with an appropriate element.
-       * Case-2 we swap one of the elements from the right of lower_x with an appropriate element.
-       */
-      lower_x -= 1; // Convert it to the index for the array A. There was an extra element added to cum_sum.
-      bool found = false;
-      for (int i = 0; i <= lower_x; i++) {
-        for (int j = lower_x + 1; j < N; j++) {
-          debug("Comparing %d and %d\n", A[i], A[j]);
-          if (
-            ((A[j] - A[i]) >= (X - cum_sum[lower_x + 1])) && ((A[j] - A[i]) <= (Y - cum_sum[lower_x + 1]))
-          ) {
-            debug("Can swap %d with %d\n", A[i], A[j]);
-            found = true;
-            break;
-          }
-        }
-        if (found) {
-          break;
-        }
-      }
-      if (found) {
-        printf("1\n");
-        continue;
-      }
-      for (int i = lower_x + 2; i < N; i++) {
-        if (
-          (A[lower_x + 1] - A[i]) >= (cum_sum[lower_x + 2] - Y) && (A[lower_x + 1] - A[i] <= (cum_sum[lower_x + 2] - X))
-        ) {
-          debug("Can swap %d with %d\n", A[lower_x + 1], A[i]);
-          found = true;
-          break;
-        }
-      }
-      if (found) {
-        printf("1\n");
-        continue;
-      }
+    } else if (one_swap) {
+      printf("1\n");
+    } else {
       printf("2\n");
     }
   }
---
+++ templates/cpp/empty.cpp
@@ -0,0 +1,67 @@
+/*
+* Problem Name/URL:
+*/
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <functional>
+#include <limits>
+#include <map>
+#include <set>
+#include <iterator>
+
+using namespace std;
+
+#define DEBUG 0
+
+#if DEBUG
+  #define debug printf
+#else
+  #define debug(...) 1 == 1
+#endif
+
+#define MAX_NODES 112345
+#define MAX_ARRAY_SIZE 112345
+#define MAX_MATRIX_SIZE 1123
+#define INFINITY_32_BIT (int)~(1<<31) // For 32-Bit
+#define INFINITY_64_BIT (long long)~(1<<63) // For 64-Bit
+#define MOD 1000000007 // MOD value
+
+// Some function names shortened
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sf scanf
+#define pf printf
+#define sd(n) scanf("%d", &n)
+#define slld(n) scanf("%lld", &n)
+#define sstr(s) scanf("%s", s)
+#define pd(n) printf("%d", n)
+#define plld(n) printf("%lld", n)
+#define pstr(s) printf("%s", s)
+#define pnl() printf("\n")
+#define psp() printf(" ")
+#define gc() getchar()
+
+// Loops
+#define rep(i, N) for (int i = 0; i < N; i++)
+#define replr(i, l, r) for (int i = l; i < r; i++)
+#define all(x) x.begin(), x.end()
+#define printvi(x) for (auto ele: x) pd(ele), psp()
+#define printvpi(x) for (auto ele: x) pd(ele.fi), psp(), pd(ele.se), pnl()
+
+// Misc
+#define pcodejam(t, answer) pf ("Case :%d %d\n", t + 1, answer)
+
+int main ()
+{
+  int T;
+
+  return 0;
+}
