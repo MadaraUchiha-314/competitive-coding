e169c87397ecd76a02815a5f7e8abbe64f8ad4ad
MadaraUchiha-314 <rohithr31@gmail.com>
2021-01-25 00:08:56 +0530

    fix: done

--- codechef/problems/COOK126B/MINIL/solution/solution.py
+++ codechef/problems/COOK126B/MINIL/solution/solution.py
@@ -3,43 +3,55 @@ if __name__ == "__main__":
     for _ in range(T):
         N, M = map(int, input().split())
         matrix = []
-        dot_star = [0 for _ in range(N)]
-        star_dot = [0 for _ in range(N)]
         for i in range(N):
             row = list(input())
             matrix.append(row)
+        
         answer_dot = 0
         answer_star = 0
+        islands_dot = 0
+        islands_star = 0
+        
         for i in range(N):
             for j in range(M):
                 if i % 2 == 0:
                     if j % 2 == 0:
-                        if matrix[i][j] == '*':
+                        islands_dot += 1
+                        if matrix[i][j] == '.':
                             answer_dot += 1
                     else:
-                        if matrix[i][j] == '.':
+                        if matrix[i][j] == '*':
                             answer_dot += 1
                 else:
-                    if j % 2 == 1:
+                    if j % 2 == 0:
                         if matrix[i][j] == '*':
                             answer_dot += 1
                     else:
+                        islands_dot += 1
                         if matrix[i][j] == '.':
-                            answer_dot += 1
+                            answer_dot += 1               
                 
                 if i % 2 == 0:
                     if j % 2 == 0:
-                        if matrix[i][j] == '.':
+                        if matrix[i][j] == '*':
                             answer_star += 1
                     else:
-                        if matrix[i][j] == '*':
+                        islands_star += 1
+                        if matrix[i][j] == '.':
                             answer_star += 1
                 else:
-                    if j % 2 == 1:
+                    if j % 2 == 0:
+                        islands_star += 1
                         if matrix[i][j] == '.':
                             answer_star += 1
                     else:
                         if matrix[i][j] == '*':
                             answer_star += 1
-        print(min(answer_dot, answer_star))
+        
+        if islands_dot > islands_star:
+            print(answer_dot)
+        elif islands_star > islands_dot:
+            print(answer_star)
+        else:
+            print(min(answer_dot, answer_star))
 
