f370f1f1c2e93b1fdbbcab62367719b27be60118
MadaraUchiha-314 <rohithr31@gmail.com>
2021-02-17 10:35:29 +0530

    fix: adding cses

---
+++ cses/problems/task/1672/solution/solution.py
@@ -0,0 +1,29 @@
+import math
+
+def floyd_warshall(N, dist):
+    for i in range(N):
+        dist[i][i] = 0
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+if __name__ == "__main__":
+    N, M, Q = map(int, input().split())
+    g = [[] for _ in range(N)]
+    dist = [[math.inf for _ in range(N)] for _ in range(N)]
+    for _ in range(M):
+        src, dest, weight = map(int, input().split())
+        src -= 1
+        dest -= 1
+        dist[src][dest] = min(weight, dist[src][dest])
+        dist[dest][src] = min(weight, dist[dest][src])
+    floyd_warshall(N, dist)
+    for _ in range(Q):
+        src, dest = map(int, input().split())
+        src -= 1
+        dest -= 1
+        if dist[src][dest] == math.inf:
+            print("-1")
+        else:
+            print(dist[src][dest])
