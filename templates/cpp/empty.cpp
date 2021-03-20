08c24c66b8f4433fd0a10dccbfa0dd7b32454066
MadaraUchiha-314 <rohithr31@gmail.com>
2021-03-20 11:11:04 +0530

    fix: adding more things in template

--- templates/cpp/empty.cpp
+++ templates/cpp/empty.cpp
@@ -1,5 +1,5 @@
 /*
- * Problem Name/URL:
+ * Problem Name/URL: 
  */
 #include <cstdio>
 #include <cstdlib>
@@ -17,6 +17,9 @@
 
 using namespace std;
 
+/**
+ * For easier debugging
+ */
 #define DEBUG 0
 
 #if DEBUG
@@ -25,7 +28,50 @@ using namespace std;
   #define debug(...) 1 == 1
 #endif
 
-int main ()
-{
+/**
+ * IO Related
+ */
+#define sf scanf
+#define sd(x) scanf("%d", &x)
+#define sll(x) scanf("%lld", &x)
+#define spd(x, y) scanf("%d %d", &x, &y)
+#define spll(x, y) scanf("%lld %lld", &x, &y)
+
+#define pf printf
+#define pd(x) printf("%d", x)
+#define pll(x) printf("%lld", x)
+#define ppd(x, y) printf("%d %d", &x, &y)
+#define ppll(x, y) printf("%lld %lld", &x, &y)
+#define pnl() printf("\n")
+#define psp() printf(" ")
+
+/**
+ * Loops
+ */
+#define rep(i, N) for (int i = 0; i < N; i++)
+#define repi(i, start, end) for (int i = start; i < end; i++)
+
+/**
+ * Misc
+ */
+#define pcodejam(t) pf("Case #%d: ", t)
+
+void input();
+void solve();
+
+/**
+ * Problem Specific Stuff
+ */
+
+
+void solve() {
+
+}
+
+int main () {
   return 0;
+}
+
+void input() {
+
 }
