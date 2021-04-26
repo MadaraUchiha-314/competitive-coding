02a051daa676dc0b4fcbc6072b8edfb8e402ce6b
MadaraUchiha-314 <rohithr31@gmail.com>
2021-04-26 14:52:15 +0530

    fix: the template is here. now only cpp

--- templates/cpp/empty.cpp
+++ templates/cpp/empty.cpp
@@ -1,11 +1,8 @@
 /*
  * Problem Name/URL: 
  */
-#include <cstdio>
+
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
 #include <vector>
 #include <queue>
 #include <stack>
@@ -15,24 +12,56 @@
 #include <map>
 #include <set>
 #include <iterator>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
 
 using namespace std;
 
-/**
- * For easier debugging
- */
-#define DEBUG 0
+#define NEW_LINE "\n"
+#define SPACE " "
+#define COMMA ","
+#define COLON ":"
+#define SQB_O "["
+#define SQB_C "]"
+#define CYB_O "{"
+#define CYB_C "}"
+#define ROB_O "("
+#define ROB_C ")"
 
-#if DEBUG
-  #define debug printf
-#else
-  #define debug(...) 1 == 1
-#endif
+/**
+ * Overloading ostream for Pairs, Vectors, Maps
+ */
+template <typename T, typename U>
+ostream& operator<< (ostream& os, pair<T, U> x) { os << ROB_O << x.first << SPACE << x.second << ROB_C; }
+
+template <typename T>
+ostream& operator<< (ostream& os, vector<T> x) { os << SQB_O; for (auto e: x) os << e << COMMA << SPACE; os << SQB_C << NEW_LINE; }
+
+template <typename T, typename U>
+ostream& operator<< (ostream& os, map<T, U> x) { os << CYB_O << NEW_LINE; for (auto e: x) os << e << COLON << SPACE << x[e] << NEW_LINE; os << CYB_C << NEW_LINE; }
+
+/**
+ * Defining min and max for multiple arguments.
+ */
+template <typename T>
+T mins(T a, T b) { return min(a, b); }
+
+template <typename T, typename... Args>
+T mins(T a, Args... args) { return min(a, mins(args...)); }
+
+template <typename T>
+T maxs(T a, T b) { return max(a, b); }
+
+template <typename T, typename... Args>
+T maxs(T a, Args... args) { return max(a, maxs(args...)); }
 
 /**
  * Misc
  */
-#define pcodejam(t) pf("Case #%d: ", t)
+#define pcodejam(t) cout << "Case #" << t << ": "
 
 void input();
 void solve();
@@ -47,7 +76,7 @@ void solve() {
 
 int main () {
   int T;
-  scanf("%d", T);
+  cin >> T;
   for (int t = 0; t < T; t++) {
     input();
     solve();
