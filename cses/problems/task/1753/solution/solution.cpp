b4cce44d9f5e12b767bc9f5d30c25b2268bf45dd
MadaraUchiha-314 <rohithr31@gmail.com>
2021-09-26 18:01:18 +0530

    fix: adding modular arithmetic and string hashing

--- cses/problems/task/1753/solution/solution.cpp
+++ cses/problems/task/1753/solution/solution.cpp
@@ -47,20 +47,62 @@ inline int mul(int a, int b, int mod) { return (a * 1ll * b) % mod; }
 inline int power(int a, int b, int mod) { int ans = 1; while (b > 0) { if (b & 1) ans = mul(ans, a, mod); a = mul(a, a, mod); b >>= 1; } return ans; }
 inline int inv(int a, int mod) { return power(a, mod - 2, mod); }
 
-/**
- * Misc
- */
-#define pcodejam(t) cout << "Case #" << t << ": "
+namespace StringHashing {
+  class StringHash {
+    public:
+      static const int mod = (int) 1e9 + 7;
+      static const int p = 9973;
+
+      static vector<int> pows;
+      static const int max_len = 1e6;
+
+      static void init() {
+        pows.resize(max_len);
+        pows[0] = 1;
+        for (int i = 1; i < max_len; i++) {
+          pows[i] = mul(pows[i - 1], p, mod);
+        }
+      }
+
+      vector<int> prefix_hash;
+
+      StringHash(string s) {
+        prefix_hash.resize(s.length() + 1);
+        prefix_hash[0] = 0;
+        for (int i = 0; i < s.length(); i++) {
+          prefix_hash[i + 1] = add(prefix_hash[i], mul((int) (s[i]), pows[i], mod), mod);
+        }
+      }
+
+      int get_hash(int l, int r) {
+        l++; r++;
+        return mul(inv(power(p, l, mod), mod), sub(prefix_hash[r], prefix_hash[l - 1], mod), mod);
+      }
+  };
+}
+
+using namespace StringHashing;
+
+vector<int> StringHash::pows;
 
 void input();
-void solve();
 
-/**
- * Problem Specific Stuff
- */
+string s, p;
 
 void solve() {
+  StringHash::init();
+  StringHash sh(s);
+  StringHash ph(p);
+  int answer = 0;
+  for (int i = 0 ; i + p.length() <= s.length(); i++) {
+    if (ph.get_hash(0, p.length() - 1) == sh.get_hash(i, i + p.length() - 1)) {
+      answer += 1;
+    }
+  }
+  cout << answer << "\n";
 }
+
+
 
 int main () {
   /**
@@ -69,8 +111,7 @@ int main () {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int T;
-  cin >> T;
+  int T = 1;
   for (int t = 0; t < T; t++) {
     input();
     solve();
@@ -79,4 +120,5 @@ int main () {
 }
 
 void input() {
+  cin >> s >> p;
 }
--- templates/cpp/empty.cpp
+++ templates/cpp/empty.cpp
@@ -5,6 +5,11 @@
 using namespace std;
 
 void input();
+
+/**
+ * Problem Specific Stuff
+ */
+
 void solve() {
 }
 
