cd022a4e5e4c8d33f52ff15769110bdeca790da5
rramakrishnan2 <Rohith_Ramakrishnan@intuit.com>
2022-07-16 13:35:36 +0530

    fix: adding generator

--- generators/generator.py
+++ generators/generator.py
@@ -19,21 +19,10 @@ def relabel_tree_nodes(size, edges):
 def change_indexing(edges, base = 1):
     return [(parent + base, child + base) for parent, child in edges]
 
-def generate_test_case(index, total_cases, file):
-    # Fill this up for each question
-    pass
-
-def generate_test_files(base_dir, total_cases):
-    files = ["in-{0}.txt".format(i) for i in range(total_cases)]
+def generate_test_files(base_dir, total_cases, generate_test_case):
+    files = ["input-{0}.txt".format(i) for i in range(total_cases)]
     for index, file in enumerate(files):
         test_file_name = "{0}/inputs/{1}".format(base_dir, file)
         os.makedirs(os.path.dirname(test_file_name), exist_ok=True)
         with open(test_file_name, "w+") as f:
             generate_test_case(index, total_cases, f)
-
-if __name__ == "__main__":
-    # Fix a new random seed for every problem
-    random.seed(1729)
-    total_cases = 10
-    # Generate test files
-    generate_test_files("", total_cases)
